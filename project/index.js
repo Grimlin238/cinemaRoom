0b4ac5427f751f33f107dd7f362c2b23ef78ab4f
Grimlin238 <ty.lashley14@icloud.com>
2024-03-03 03:26:55 -0500

    App now pulls movies from TMDB API. Removed ApiHandler module.

--- project/index.js
+++ project/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const dbh = require('DbHandler')
-
 const app = express();
 
 app.use(express.urlencoded({
@@ -25,9 +24,36 @@ app.get('/home', (req, res) => {
 	res.send('<nav> <a href="/home"> Home </a> <a href="/latest"> Latest Movies </a> <a href="/search"> Search For Movies </a> </nav>');
 })
 
+app.get('/latest', async (req, res) => {
+    let page = '<nav> <a href="/home"> Home </a> <a href="/latest"> Latest Movies </a> <a href="/search"> Search </a> </nav> <h1> Latest Movies </h1>';
+
+    try {
+        const response = await fetch("https://api.themoviedb.org/3/movie/latest?api_key=a26a1684ba14b7b49ebbd51f98eb95f7");
+        const data = await response.json();
+        const movie = data
+			
+            page += `<h1>${movie.original_title}</h1>`;
+            if (movie.poster_path) {
+                page += `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.original_title}">`;
+            } else {
+                page += "<p>No image for movie</p>";
+            }
+
+        res.send(page);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching latest movies');
+    }
+});
+
 app.get('/login', (req, res) => {
 	
-res.send('<h1> Glad you could join us. Create an account below </h1> <form method="post"> <h1> Username? </h1> <input name="username"> <h1> password? </h1> <input type="password" name="password"> <button> Log in </button> </form>')
+res.send('<h1> Welcome back! </h1> <form method="post"> <h1> Username? </h1> <input name="username"> <h1> password? </h1> <input type="password" name="password"> <button> Log in </button> </form>')
+})
+
+app.get('/search', (req, res) => {
+	res.send('<nav> <a href="/home"> Home </a> <a href="/latest"> Latest Movies </a> <a href="/search"> Search </a> </nav> <form method="post"> <h1> Search For Movies </h1> <input name="movieSearch"> <button> Search </button> </form>')
+	
 })
 
 app.post('/create', async (req, res) => {
@@ -65,7 +91,38 @@ app.post('/login', async (req, res) => {
 	}
 })
 
-app.listen(8080, () => {
-	
+app.post('/search', async (req, res) => {
+    let searchPage = '<nav> <a href="/search"> Back To Search </a> </nav>  <h1> Search results </h1>';
+    
+    try {
+        const search = req.body.movieSearch;
+        
+        const response = await fetch('https://api.themoviedb.org/3/search/movie?api_key=a26a1684ba14b7b49ebbd51f98eb95f7&query=' + search);
+        
+        const data = await response.json();
+        
+        const movies = data.results; // The results array contains the movie data
+        
+        if (movies && movies.length > 0) {
+            movies.slice(0, 12).forEach(movie => {
+                searchPage += `<h1>${movie.original_title}</h1>`;
+                
+                if (movie.poster_path) {
+                    searchPage += `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.original_title}">`;
+                } else {
+                    searchPage += "<p>No image for movie</p>";
+                }
+            });
+            res.send(searchPage);
+        } else {
+            res.send('<nav> <a href="/search"> Back To Search </a> </nav> <p> We could not find that movie. </p>');
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error fetching search results');
+    }
+});
+
+app.listen(8080, () => {	
 	console.log("I\'m listening on port 8080");
 })
